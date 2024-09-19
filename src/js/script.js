163eb67a9775952f2adba8972ef31794362760d9
AstaHatake <danielmzpro@gmail.com>
2024-09-18 22:05:38 -0400

    Creada la funcion de copiar en el portapapeles la contraseña creada

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,12 @@
 // Variables
 
-let checkBoxSignos = document.getElementById("checkbox-signos");
-let checkBoxNumeros = document.getElementById("checkbox-numeros");
-let checkBoxLetras = document.getElementById("checkbox-letras");
-let rangeLenght = document.getElementById("rangeLenght");
-let spanLenght = document.getElementById("spanLenght");
+const checkBoxSignos = document.getElementById("checkbox-signos");
+const checkBoxNumeros = document.getElementById("checkbox-numeros");
+const checkBoxLetras = document.getElementById("checkbox-letras");
+const rangeLenght = document.getElementById("rangeLenght");
+const spanLenght = document.getElementById("spanLenght");
+const inputRetry = document.querySelector(".input-retry");
+const buttonCreate = document.getElementById("buttonCreate");
 
 const passwordCreateHTML = document.getElementById("password");
 
@@ -12,6 +14,10 @@ let password;
 
 
 function generatePassword(rangeLenght, useUppercase, useNumbers, useSymbols) {
+
+    document.querySelector(".input-retry").style = "display: flex; opacity: 1"
+
+    buttonCreate.style.display = "none"; 
 
     let chars = '';
     if (useUppercase) {
@@ -26,7 +32,7 @@ function generatePassword(rangeLenght, useUppercase, useNumbers, useSymbols) {
         chars += '!@#$%^&*()_+-={}:<>?';
     }
     chars += ' ';
-    let password = '';
+    password = '';
 
     console.log(rangeLenght)
 
@@ -46,7 +52,7 @@ function generatePassword(rangeLenght, useUppercase, useNumbers, useSymbols) {
 function showError(){
     document.querySelector(".input-retry").style = "display: flex; opacity: 1"
 
-    document.getElementById("buttonCreate").style.display = "none"; 
+    buttonCreate.style.display = "none"; 
 
     document.querySelectorAll(".checkbox-label").forEach(checkBoxLabel => {
         checkBoxLabel.style.color = '#ff0000';
@@ -58,19 +64,48 @@ function showError(){
 
 }
 
+function copyPassword (text){
+    if (text) {
+        if (navigator.clipboard) {
+            navigator.clipboard.writeText(text).then(()=>{
+                console.log("Texto copiado correctamente");
+            }).catch((error)=>{
+                console.error("Error al copiar texto al portapapeles:", error);
+            })
+    
+            passwordCreateHTML.style.backgroundColor = "#01ffff93";
+            passwordCreateHTML.style.outline = "none !important";
+            passwordCreateHTML.style.border = "none !important";
+    
+            passwordCreateHTML.value = "COPIADA!"
+
+            setTimeout(()=>{
+                passwordCreateHTML.style.backgroundColor = "transparent";
+                passwordCreateHTML.value = text;
+            },300)
+    
+        } else {
+            console.error("La API de portapapeles no esta disponible:");
+        }
+    }
+}
+
 
 rangeLenght.addEventListener("mousemove", ()=>{
-    document.getElementById("spanLenght").innerHTML = "LONGITUD : " + rangeLenght.value; 
+    spanLenght.innerHTML = "LONGITUD : " + rangeLenght.value; 
 });
 
+
 rangeLenght.addEventListener('mouseout', ()=>{
-    document.getElementById("spanLenght").innerHTML = "LONGITUD : " + rangeLenght.value; 
+    spanLenght.innerHTML = "LONGITUD : " + rangeLenght.value; 
 })
 
-document.getElementById("buttonCreate").addEventListener("click", ()=>{
+
+buttonCreate.addEventListener("click", ()=>{
     if (checkBoxLetras.checked == true || checkBoxNumeros.checked == true  ||checkBoxSignos.checked == true ) {
         password = generatePassword(rangeLenght.value, checkBoxLetras.checked,checkBoxNumeros.checked,checkBoxSignos.checked);
         passwordCreateHTML.value = password;
+
 
     }
 
@@ -80,15 +115,20 @@ document.getElementById("buttonCreate").addEventListener("click", ()=>{
 
 })
 
-document.querySelector(".input-retry").addEventListener("click", ()=>{
+
+inputRetry.addEventListener("click", ()=>{
     if (checkBoxLetras.checked == true || checkBoxNumeros.checked == true  ||checkBoxSignos.checked == true ) {
         password = generatePassword(rangeLenght.value, checkBoxLetras.checked,checkBoxNumeros.checked,checkBoxSignos.checked);
         passwordCreateHTML.value = password;
-
     }
+
 
     else {
         showError();
     }
 
+})
+
+passwordCreateHTML.addEventListener("click",()=>{
+    copyPassword(password);
 })
